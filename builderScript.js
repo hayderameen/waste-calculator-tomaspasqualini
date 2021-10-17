9c7ddd11329fde57a4b0b824d98d4e82715ee454
hayderameen <hayder204@gmail.com>
2021-10-17 09:40:13 +0500

    For webflow

--- builderScript.js
+++ builderScript.js
@@ -8,7 +8,6 @@ document.getElementById(
     <p class="submitWarning">Please enter only numbers</p>
     <!-- change to a number input -->
     <span class="dollar_sign">$</span>
-    <label for="totalRevenue"></label>
     <input
       type="number"
       id="totalRevenue"
